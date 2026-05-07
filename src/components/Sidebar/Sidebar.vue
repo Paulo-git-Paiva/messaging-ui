99c6ba041f9166d44e8bed448e624584d82e86aa
Paulo Paiva <paulo.h.paiva.c@gmail.com>
2026-05-07 00:20:28 -0300

    feat/ Implement sidebar conversations

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <aside class="sidebar">
+    <SidebarHeader />
+    <ConversationList />
+  </aside>
+</template>
+
+<script setup lang="ts">
+import SidebarHeader from "./SidebarHeader.vue";
+import ConversationList from "./ConversationList.vue";
+</script>
