84e8245897a4ce446044c3b5ef129c1a71b9913f
Paulo Paiva <paulo.h.paiva.c@gmail.com>
2026-05-07 00:25:02 -0300

    feat/ implement chat messages area

---
+++ src/components/Chat/ChatHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header v-if="conversation" class="chat-header">
+    <div class="chat-user">
+      <Avatar :src="conversation.avatar" />
+      <h2>{{ conversation.name }}</h2>
+    </div>
+
+    <button class="archive-btn">
+      <Archive :size="16" />
+      Arquivar
+    </button>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { Archive } from "lucide-vue-next";
+import { useChatStore } from "@/store/chat";
+import Avatar from "../UI/Avatar.vue";
+
+const store = useChatStore();
+
+const conversation = computed(() => {
+  return store.activeConversation;
+});
+</script>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="chat-view">
+    <ChatHeader />
+    <MessageList />
+    <MessageInput />
+  </main>
+</template>
+
+<script setup lang="ts">
+import ChatHeader from "@/components/Chat/ChatHeader.vue";
+import MessageList from "@/components/Chat/MessageList.vue";
+import MessageInput from "@/components/Input/MessageInput.vue";
+</script>
